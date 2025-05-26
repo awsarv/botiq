282e5842a880eb2f78665bf614edae88f7d408a6
awsarv <awsarv@gmail.com>
2025-05-27 00:49:20 +0530

    Update outputs.tf

--- Terraform/azure/gamma/outputs.tf
+++ Terraform/azure/gamma/outputs.tf
@@ -11,3 +11,7 @@ output "aks_cluster_name" {
 output "oidc_issuer_url" {
   value = azurerm_kubernetes_cluster.aks.oidc_issuer_url
 }
+
+output "selected_kubernetes_version" {
+  value = local.selected_version
+}
